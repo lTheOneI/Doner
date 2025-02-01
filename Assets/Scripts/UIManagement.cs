73ce2e198efb0adbfe52814075ba9524727da42a
Chu Quý <quyngoquyham@gmail.com>
2025-02-01 19:20:35 +0700

    01/02/2025
    Added Music and Sounds to Game, Musics acn be played randomly

--- Assets/Scripts/UIManagement.cs
+++ Assets/Scripts/UIManagement.cs
@@ -9,25 +9,63 @@ public class UIManagement : MonoBehaviour
 
     [Header("UI Objects")]
     public Button playBtn;
+    public GameObject settingPanel;
     public Slider musicSlider;
     public Slider soundSlider;
 
     [Header("Sounds")]
     public AudioSource[] musicBG;
     public AudioSource sound;
+
+    private bool isPaused;
+    private int currenTrackIndex = -1;
     void Start()
     {
         playBtn.onClick.AddListener(StartGame);
 
         sound.volume = soundSlider.value;
         soundSlider.onValueChanged.AddListener(SetVolume);
+
+        //BG Music play randomly
+        if (musicBG.Length > 0)
+        {
+            PlayRandomTrack();
+        }
     }
 
     void Update()
     {
-        
+        if (!musicBG[currenTrackIndex].isPlaying)
+        {
+            PlayRandomTrack();
+        }
+        if (settingPanel.activeSelf)
+        {
+            PauseGame();
+        }
+        else
+        {
+            ResumeGame();
+        }
     }
 
+    void PlayRandomTrack()
+    {
+        int newTrackIndex;
+        do
+        {
+            newTrackIndex = Random.Range(0, musicBG.Length);
+        }
+        while (newTrackIndex == currenTrackIndex);
+        if (newTrackIndex == currenTrackIndex )
+        {
+            musicBG[currenTrackIndex].Stop();
+        }
+
+        currenTrackIndex = newTrackIndex;
+        musicBG[currenTrackIndex].Play();
+
+    }
     public void StartGame()
     {
         SceneManager.LoadScene("MainGame");
@@ -37,5 +75,17 @@ public class UIManagement : MonoBehaviour
     {
         sound.volume = volume;
         Debug.Log("sound Volume" + sound.volume );
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        isPaused = false;
     }
 }
